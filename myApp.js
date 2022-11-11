697a63f38f177e9000e5d680e2fbecf7a7c742a4
ramos-vini <ramoscontrole@gmail.com>
2022-11-11 12:03:52 -0300

    Serve an HTML File

--- myApp.js
+++ myApp.js
@@ -3,11 +3,13 @@ let app = express();
 
 console.log("Hello World");
 
+// app.get("/", function (req, res) {
+//     res.send('Hello Express');
+// });
+
 app.get("/", function (req, res) {
-    res.send('Hello Express');
+    res.send(`${__dirname}/views/index.html`);
 });
-
-
 
 
 
