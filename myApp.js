ed9e660e1829ed742e116752d9b2467a52d04327
ramos-vini <ramoscontrole@gmail.com>
2022-11-11 12:51:00 -0300

    Serve JSON on a Specific Route

--- myApp.js
+++ myApp.js
@@ -7,8 +7,12 @@ app.get("/", function (req, res) {
     // res.send('Hello Express');
 
     // res.send(`${__dirname}/views/index.html`);
-    
+
     res.send(app.use(`${__dirname}/public`, express.static(`${__dirname}/public/style.css`)));
+});
+
+app.get("/json", function (req, res) {
+    res.json({"message": "Hello json"});
 });
 
 
